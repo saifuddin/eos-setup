4fac54980999c8e79017ba1878541f90bad79d03
Saifuddin Fakhri <saifuddin.fakhri@gmail.com>
2024-04-20 06:35:00 -0500

    cc

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install Vim
+sudo apt install vim -y
+
+# Install Firefox
+sudo apt install firefox -y
+
+# Install Git
+sudo apt install git -y
+
+# Remove Photos
+sudo apt remove io.elementary.photos -y
+
+# Remove Code (assuming you mean the Code editor)
+sudo apt remove io.elementary.code -y
+
+# Remove Camera
+sudo apt remove io.elementary.camera -y
+
+# Remove Calendar
+sudo apt remove io.elementary.calendar -y
+
+# Remove Music
+sudo apt remove io.elementary.music -y
+
+# Remove Multitasking View
+sudo apt remove io.elementary.switchboard-plug-multitasking -y
+
+# Remove Shortcuts
+sudo apt remove io.elementary.shortcut-overlay -y
+
+# Remove Videos
+sudo apt remove io.elementary.videos -y
+
+# Remove Web
+sudo apt remove epiphany-browser -y
